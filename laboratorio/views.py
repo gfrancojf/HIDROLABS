323fb71534607f8b5f46001bd1f6251c75d641bd
Frank Dev <gfrancojf@gmail.com>
2023-05-13 11:27:35 -0400

    Integrando layouts Adminlte

--- laboratorio/views.py
+++ laboratorio/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, get_object_or_404
+from .models import Muestra
 
-# Create your views here.
+class MuestraDetailView(View):
+    def get(self, request, muestra_id):
+        muestra = get_object_or_404(Muestra, id=muestra_id)
+        return render(request, 'laboratorio/muestra_detalle.html', {'muestra': muestra})
